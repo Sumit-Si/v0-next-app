31b5ce3f4ea40aef67a7abd46311c1f36ca4834a
Sumit <pankajsinghtomar987@gmail.com>
2026-02-01 22:12:12 +0530

    add dark mode, create homepage with navbar, project-form component and fix layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,17 @@
 import { onBoardUser } from "@/modules/auth/actions";
+import Navbar from "@/modules/home/components/navbar";
 import React from "react";
 
 const RootLayout = async ({ children }: React.ComponentProps<"main">) => {
   await onBoardUser();
 
   return (
-    <main className="flex flex-col min-h-screen relative overflow-x-hidden border">
+    <main className="flex flex-col min-h-screen relative overflow-x-hidden">
       {/* Navbar  */}
+      <Navbar />
       <div className="fixed inset-0 -z-10 h-full w-full bg-background dark:bg-[radial-gradient(#dadde2_1px, transparent_1px)] bg-size:[16px_16px]" />
 
-      <div className="flex-1 w-full mt-20">
+      <div className="flex-1 w-full my-20">
         {children}
       </div>
     </main>
--- app/(root)/sign-up/[[...sign-up]]/page.tsx
+++ app/(root)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SignUpPage = () => {
   return (
-    <div className="flex flex-col max-w-3xl mx-auto w-full">
+    <div className="flex flex-col items-center max-w-3xl mx-auto w-full">
         <section className="space-y-6 pt-[16vh] 2xl:pt-48">
             <SignUp />
         </section>
